f80d44957139e1c1e7ca8e7a02f18ef192ca9a95
Fiveighter <43967642+Fiveighter@users.noreply.github.com>
2021-09-22 10:23:24 -0300

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const server = express();
+const routes = require("./routes/index");
+const nunjucks = require("nunjucks");
+const methodOverride = require("method-override");
+const path = require("path");
+const session = require("./config/session");
+
+server.use(session);
+server.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+server.use(express.static("public"));
+server.use(express.urlencoded({ extended: true }));
+server.use(methodOverride("_method"));
+server.set("view engine", "html");
+nunjucks.configure(path.join(__dirname, "app/views"), {
+  express: server,
+  autoescape: false,
+  noCache: true,
+});
+server.use(routes);
+
+server.use(function (req, res) {
+  res.status(404).redirect("/");
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, function () {
+  console.log(`Server running in http://127.0.0.1:${PORT}`);
+});
